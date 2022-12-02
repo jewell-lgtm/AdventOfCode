b4b9e26a0a5c3164eff124243cf96c48e06d8f16
Matt Jewell <m@mattjewell.co.uk>
2022-12-02 06:24:05 +0100

    22: day 2

--- src/test/kotlin/days/y2022/Day02Test.kt
+++ src/test/kotlin/days/y2022/Day02Test.kt
@@ -11,41 +11,125 @@ class Day02Test {
 
     @Test
     fun testExampleOne() {
-        assertThat(Day02().partOne("""
-            1
-            2
-            3
-        """.trimIndent()), `is`(-1))
+        assertThat(
+            Day02().partOne(
+                """
+            A Y
+            B X
+            C Z
+        """.trimIndent()
+            ), `is`(15)
+        )
     }
 
     @Test
     fun testPartOne() {
-        assertThat(Day02().partOne(), `is`(-1))
+        assertThat(Day02().partOne(), `is`(14264))
     }
 
     @Test
     fun testExampleTwo() {
-        assertThat(Day02().partTwo("""
-            1
-            2
-            3
-        """.trimIndent()), `is`(-1))
+        assertThat(
+            Day02().partTwo(
+                """
+            A Y
+            B X
+            C Z
+        """.trimIndent()
+            ), `is`(12)
+        )
     }
 
     @Test
     fun testPartTwo() {
-        assertThat(Day02().partTwo(), `is`(-1))
+        assertThat(Day02().partTwo(), `is`(12382))
     }
 }
 
 
-
-class Day02 : Day(2022, 0) {
+class Day02 : Day(2022, 2) {
     override fun partOne(input: String): Any {
-        return -1
+        val throws = parseInput(input)
+        return throws.sumOf { it.partOneScore() }
     }
+
 
     override fun partTwo(input: String): Any {
-        return -1
+        val throws = parseInput(input)
+        return throws.sumOf { it.partTwoScore() }
+    }
+
+
+    private fun parseInput(input: String): List<Pair<Char, Char>> = input.lines().map { line ->
+        val (a, b) = line.split(" ")
+        Pair(a[0], b[0])
     }
 }
+
+
+private fun Pair<Char, Char>.partOneScore(): Int =
+    when (first) {
+        'A' -> when (second) {
+            'X' -> 1 + 3
+            'Y' -> 2 + 6
+            'Z' -> 3 + 0
+            else -> error("Unknown second: $second")
+        }
+
+        'B' -> when (second) {
+            'X' -> 1 + 0
+            'Y' -> 2 + 3
+            'Z' -> 3 + 6
+            else -> error("Unknown second: $second")
+        }
+
+        'C' -> when (second) {
+            'X' -> 1 + 6
+            'Y' -> 2 + 0
+            'Z' -> 3 + 3
+            else -> error("Unknown second: $second")
+        }
+
+        else -> error("Unknown first: $first")
+    }
+
+
+private fun Pair<Char, Char>.partTwoScore(): Int =
+    when (first) {
+        'A' -> when (second) {
+            'X' -> Pair('A', 'Z').partOneScore()
+            'Y' -> Pair('A', 'X').partOneScore()
+            'Z' -> Pair('A', 'Y').partOneScore()
+            else -> error("Unknown second: $second")
+        }
+
+        'B' -> when (second) {
+            'X' -> Pair('B', 'X').partOneScore()
+            'Y' -> Pair('B', 'Y').partOneScore()
+            'Z' -> Pair('B', 'Z').partOneScore()
+            else -> error("Unknown second: $second")
+        }
+
+        'C' -> when (second) {
+            'X' -> Pair('C', 'Y').partOneScore()
+            'Y' -> Pair('C', 'Z').partOneScore()
+            'Z' -> Pair('C', 'X').partOneScore()
+            else -> error("Unknown second: $second")
+        }
+
+        else -> error("Unknown first: $first")
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
