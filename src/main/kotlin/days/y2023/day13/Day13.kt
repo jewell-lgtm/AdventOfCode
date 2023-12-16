506e38edf8a7b8617fd6298ad878a4220f2eff4a
Matt Jewell <m@mattjewell.co.uk>
2023-12-16 12:14:31 +0100

    Day 13 something something

--- src/main/kotlin/days/y2023/day13/Day13.kt
+++ src/main/kotlin/days/y2023/day13/Day13.kt
@@ -11,7 +11,32 @@ class Day13(val input: PuzzleInput) {
     fun partOne(): Int {
         return maps.sumOf { it.score() }
     }
+
+    fun partTwo(): Int {
+        for (map in maps) {
+            val y = map.yReflections().toSet()
+            val x = map.xReflections().toSet()
+
+            val firstDifference = map.smudgeOne().filter { smudged ->
+                (smudged.yReflections().toSet() - y).isNotEmpty() ||
+                        (smudged.xReflections().toSet() - x).isNotEmpty()
+            }.toList()
+
+            if (firstDifference.size != 1) {
+                error("No difference found")
+            }
+
+            for (smudged in map.smudgeOne()) {
+                if (smudged.score() == 0) {
+                    return smudged.chars.sumOf { line -> line.count { it == '#' } }
+                }
+            }
+        }
+
+        return -1
+    }
 }
+
 
 fun String.toPuzzleMaps(): List<PuzzleMap> {
     val chunks = this.split("\n\n")
@@ -30,19 +55,20 @@ data class PuzzleMap(val chars: List<List<Char>>) {
 }
 
 fun PuzzleMap.score(): Int {
-    val yReflections = (1 until chars.first().size).mapNotNull { y ->
-        if (reflectAboutY(y) == this) y else null
-    }
-    val xReflections = (1 until chars.size).mapNotNull { x ->
-        if (reflectAboutX(x) == this) x else null
-    }
-
     // To summarize your pattern notes, add up the number of columns to the left of each vertical line of reflection;
     // to that, also add 100 multiplied by the number of rows above each horizontal line of reflection.
-    val yScore = yReflections.sumOf { it }
-    val xScore = xReflections.sumOf { 100 * it }
+    val yScore = yReflections().sumOf { it }
+    val xScore = xReflections().sumOf { 100 * it }
 
     return yScore + xScore
+}
+
+private fun PuzzleMap.xReflections() = (1 until chars.size).mapNotNull { x ->
+    if (reflectAboutX(x) == this) x else null
+}
+
+private fun PuzzleMap.yReflections() = (1 until chars.first().size).mapNotNull { y ->
+    if (reflectAboutY(y) == this) y else null
 }
 
 fun PuzzleMap.reflectAboutY(y: Int): PuzzleMap {
@@ -73,6 +99,21 @@ private fun <E> List<E>.splitAt(y: Int): Pair<List<E>, List<E>> {
     return Pair(before, after)
 }
 
+private fun PuzzleMap.smudgeOne(): Sequence<PuzzleMap> = sequence {
+    for (y in chars.indices) {
+        for (x in chars.first().indices) {
+            yield(smudge(y, x))
+        }
+    }
+}
+
+fun PuzzleMap.smudge(y: Int, x: Int) = copy(chars = chars.smudge(y, x))
+
+fun List<List<Char>>.smudge(y: Int, x: Int): List<List<Char>> {
+    val result = this.toMutableList().map { it.toMutableList() }
+    result[y][x] = if (result[y][x] == '.') '#' else '.'
+    return result
+}
 
 fun main() {
     val year = 2023
@@ -82,14 +123,14 @@ fun main() {
     val puzzleInput: PuzzleInput = InputReader.getPuzzleInput(year, day)
 
     fun partOne(input: PuzzleInput) = Day13(input).partOne()
+    fun partTwo(input: PuzzleInput) = Day13(input).partTwo()
 
-
-    println(exampleInput.toPuzzleMaps()[0])
 
 
 
 
     println("Example 1: ${partOne(exampleInput)}")
     println("Puzzle 1: ${partOne(puzzleInput)}")
+    println("Example 2: ${partTwo(exampleInput)}")
 }
 
