643a79039c8fef1531c657fc3c52092b04cd694d
Matt Jewell <m@mattjewell.co.uk>
2023-12-23 18:17:51 +0000

    day 23 part one

--- src/main/kotlin/days/y2023/day23/Day23.kt
+++ src/main/kotlin/days/y2023/day23/Day23.kt
@@ -2,117 +2,81 @@ package days.y2023.day23
 
 import util.InputReader
 
-
 class Day23(val input: List<String>) {
-    val start = Position(0, input.first().indexOfOnly('.'))
-    val end = Position(input.size - 1, input.last().indexOfOnly('.'))
-    val neighbors =
-        input.flatMapIndexed { y: Int, row: String ->
-            row.indices.map { x ->
-                Position(y, x) to input.neighbors(
-                    Position(y, x)
-                )
-            }
-        }.toMap()
-    val cells = input.flatMapIndexed { y: Int, row: String ->
+    private val start = Position(0, input.first().indexOfOnly('.'))
+    private val end = Position(input.size - 1, input.last().indexOfOnly('.'))
+    private val neighbors = input.flatMapIndexed { y, row ->
+        row.indices.map { x ->
+            Position(y, x) to input.neighbors(Position(y, x))
+        }
+    }.toMap()
+    private val cells = input.flatMapIndexed { y, row ->
         row.indices.map { x ->
             Position(y, x) to row[x]
         }
     }.toMap()
 
+    fun partOne(): Int = longestPath(true, start, setOf(start))
+    fun partTwo(): Int = longestPath(false, start, setOf(start))
 
-    fun partOne(): Int {
-        return longestPath(true, start, 0, setOf(start))
-    }
-
-    fun partTwo(): Int {
-        return longestPath(false, start, 0, setOf(start))
-    }
-
-
-
-
-    private fun longestPath(slipperyHills: Boolean, from: Position, stepsTaken: Int, visited: Set<Position>): Int {
-        if (from == end) return stepsTaken
+    private fun longestPath(slipperyHills: Boolean, from: Position, visited: Set<Position>): Int {
+        if (from == end) return visited.size - 1
         val unvisited = from.neighbors().filter { it !in visited }
         if (unvisited.isEmpty()) return -1
 
-        if (!slipperyHills) {
-            return unvisited.maxOf { longestPath(false, it, stepsTaken + 1, visited + it) }
+        return if (!slipperyHills) {
+            unvisited.maxOfOrNull { longestPath(false, it, visited + it) } ?: -1
+        } else {
+            unvisited.mapNotNull { next ->
+                when (cells.getValue(next)) {
+                    '.' -> longestPath(true, next, visited + next)
+                    '>' -> if (next.right() !in visited) longestPath(
+                        true,
+                        next.right(),
+                        visited + next + next.right()
+                    ) else null
+                    '<' -> if (next.left() !in visited) longestPath(
+                        true,
+                        next.left(),
+                        visited + next + next.left()
+                    ) else null
+                    '^' -> if (next.up() !in visited) longestPath(
+                        true,
+                        next.up(),
+                        visited + next + next.up()
+                    ) else null
+                    'v' -> if (next.down() !in visited) longestPath(
+                        true,
+                        next.down(),
+                        visited + next + next.down()
+                    ) else null
+                    else -> null
+                }
+            }.max()
         }
-
-        return unvisited.mapNotNull {
-            when (cells.getValue(it)) {
-                '.' -> if (it !in visited) longestPath(true, it, stepsTaken + 1, visited + it) else null
-
-                '>' -> if (it !in visited && it.right() !in visited) longestPath(
-                    true,
-                    it.right(),
-                    stepsTaken + 2,
-                    visited + it + it.right()
-                ) else null
-
-                '<' -> if (it !in visited && it.left() !in visited) longestPath(
-                    true,
-                    it.left(),
-                    stepsTaken + 2,
-                    visited + it + it.left()
-                ) else null
-
-                '^' -> if (it !in visited && it.up() !in visited) longestPath(
-                    true,
-                    it.up(),
-                    stepsTaken + 2,
-                    visited + it + it.up()
-                ) else null
-
-                'v' -> if (it !in visited && it.down() !in visited) longestPath(
-                    true,
-                    it.down(),
-                    stepsTaken + 2,
-                    visited + it + it.down()
-                ) else null
-
-                else -> TODO("$it")
-            }
-        }.max()
     }
-
 
     data class Position(val y: Int, val x: Int)
 
-
-    fun String.indexOfOnly(char: Char): Int {
-        require(count { it == char } == 1)
+    private fun String.indexOfOnly(char: Char): Int {
+        require(count { it == char } == 1) { "The character $char should only appear once." }
         return indexOf(char)
     }
 
-    fun Position.neighbors(): Set<Position> = neighbors.getValue(this)
-
-    fun Position.down(): Position = copy(y = y + 1)
-
-    fun Position.up(): Position = copy(y = y - 1)
-
-    fun Position.left(): Position = copy(x = x - 1)
-
-    fun Position.right(): Position = copy(x = x + 1)
+    private fun Position.neighbors(): Set<Position> = neighbors.getValue(this)
+    private fun Position.down() = copy(y = y + 1)
+    private fun Position.up() = copy(y = y - 1)
+    private fun Position.left() = copy(x = x - 1)
+    private fun Position.right() = copy(x = x + 1)
 
     private fun List<String>.neighbors(position: Position): Set<Position> {
         return listOf(
-            position.copy(y = position.y - 1),
-            position.copy(y = position.y + 1),
-            position.copy(x = position.x - 1),
-            position.copy(x = position.x + 1)
+            position.up(), position.down(), position.left(), position.right()
         ).filter {
-            it.y in this.indices &&
-                    it.x in this[it.y].indices &&
-                    this[it.y][it.x] != '#'
+            it.y in indices && it.x in this[it.y].indices && this[it.y][it.x] != '#'
         }.toSet()
     }
-
-
 }
-
 
 fun main() {
     val year = 2023
@@ -129,4 +93,3 @@ fun main() {
     println("Example 2: ${partTwo(exampleInput)}")
     println("Puzzle 2: ${partTwo(puzzleInput)}")
 }
-
